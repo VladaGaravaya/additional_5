494f8336b8e0b467c5e6cfc118c4986dadee930e
VladaGaravaya <36517813+VladaGaravaya@users.noreply.github.com>
2018-09-17 21:36:03 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        let opening = {
+            '[': ']',
+            '{': '}',
+            '(': ')',
+            '|': '|',
+            '1':'2',
+            '3':'4',
+            '5':'6',
+            '7':'7',
+            '8':'8',
+        };
+
+        let stack = [];
+
+        for(let i = 0; i < str.length; i++){
+            let c = str[i];
+
+            if(opening[c]){
+                stack.push(c);
+            } else {
+                if(stack.length === 0){
+                    return false;
+                }
+
+                let br = stack.pop();
+
+                if(c!==opening[br]){
+                    return false;
+                }
+            }
+
+        }
+        if (stack.length > 0){
+            return false;
+        } else if(stack.length==0){
+            return true;
+        }
 }
